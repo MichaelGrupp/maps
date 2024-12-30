f690121fd3b97075541d1a2aca409ec286fd664a
Michael Grupp <michael.grupp@tum.de>
2024-12-30 01:56:09 +0100

    Consider map visibility when flipping through lenses

--- src/app.rs
+++ src/app.rs
@@ -138,6 +138,34 @@ impl AppState {
         }
     }
 
+    fn flip_lens(&mut self) {
+        if !self.options.lens.enabled || self.options.view_mode != ViewMode::Aligned {
+            // Activate in all cases, or toggle in non-overlapping views.
+            self.options.lens.enabled = !self.options.lens.enabled;
+            return;
+        }
+        // In overlapping aligned view, switch to the next visible maps, then disable.
+        let keys: Vec<&String> = self
+            .maps
+            .keys()
+            .filter(|k| self.maps.get(*k).unwrap().visible)
+            .collect();
+        if keys.is_empty() {
+            self.options.lens.enabled = false;
+            return;
+        }
+        let active_lens = self.options.active_lens.get_or_insert(keys[0].clone());
+        if let Some(active_index) = keys.iter().position(|k| *k == active_lens) {
+            let next_index = (active_index + 1) % keys.len();
+            if next_index == 0 {
+                self.options.active_lens = None;
+                self.options.lens.enabled = false;
+            } else {
+                *active_lens = keys[next_index].clone();
+            }
+        }
+    }
+
     fn handle_key_shortcuts(&mut self, ui: &egui::Ui) {
         ui.input(|i| {
             if i.key_released(egui::Key::Escape) {
@@ -145,24 +173,7 @@ impl AppState {
                 self.options.settings_visible = false;
                 self.options.lens.enabled = false;
             } else if i.key_released(egui::Key::L) || i.pointer.secondary_released() {
-                // Activate if not enabled.
-                // If enabled, switch to the next map until the last one, then disable.
-                if self.options.lens.enabled && self.options.view_mode == ViewMode::Aligned {
-                    let keys: Vec<String> = self.maps.keys().cloned().collect();
-                    if !keys.is_empty() {
-                        let active_lens = self.options.active_lens.get_or_insert(keys[0].clone());
-                        let active_index = keys.iter().position(|k| k == active_lens).unwrap();
-                        let next_index = (active_index + 1) % keys.len();
-                        if next_index == 0 {
-                            self.options.active_lens = None;
-                            self.options.lens.enabled = false;
-                        } else {
-                            *active_lens = keys[next_index].clone();
-                        }
-                    }
-                } else {
-                    self.options.lens.enabled = !self.options.lens.enabled;
-                }
+                self.flip_lens();
             }
             if i.key_released(egui::Key::M) {
                 self.options.menu_visible = !self.options.menu_visible;
