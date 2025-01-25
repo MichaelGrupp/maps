bd95a3481ef0e55aa1f41c80612e4f30bcc977cc
Michael Grupp <michael.grupp@tum.de>
2025-01-25 11:44:39 +0100

    Wrap menu content in scroll area

--- src/app_impl/menu_panel.rs
+++ src/app_impl/menu_panel.rs
@@ -9,41 +9,46 @@ impl AppState {
             return;
         }
         egui::SidePanel::left("menu").show(ui.ctx(), |ui| {
-            ui.heading("Maps");
-            ui.add_space(SPACE);
-            self.load_meta_button(ui);
-            ui.separator();
-            let mut to_delete: Vec<String> = Vec::new();
-            egui::Grid::new("maps_list")
-                .num_columns(2)
-                .striped(true)
-                .show(ui, |ui| {
-                    for (name, map) in &mut self.maps {
-                        if ui.checkbox(&mut map.visible, name).changed() {
-                            self.tile_manager.set_visible(name, map.visible);
-                        }
-                        if ui.button("🗑").on_hover_text("Delete Map").clicked() {
-                            to_delete.push(name.clone());
-                        }
-                        ui.end_row();
-                    }
-                });
-            self.delete(&to_delete);
-
-            if self.maps.is_empty() {
-                return;
-            }
-
-            ui.separator();
-            ui.add_space(SPACE);
-            ui.heading("Pose");
-            ui.add_space(SPACE);
-            self.pose_edit(ui);
-            if !self.options.pose_edit.selected_map.is_empty() && self.maps.len() > 1 {
-                ui.separator();
-                ui.add_space(SPACE);
-                self.apply_pose_to_other_maps(ui);
-            }
+            egui::ScrollArea::vertical().show(ui, |ui| {
+                self.menu_content(ui);
+            });
         });
+    }
+    fn menu_content(&mut self, ui: &mut egui::Ui) {
+        ui.heading("Maps");
+        ui.add_space(SPACE);
+        self.load_meta_button(ui);
+        ui.separator();
+        let mut to_delete: Vec<String> = Vec::new();
+        egui::Grid::new("maps_list")
+            .num_columns(2)
+            .striped(true)
+            .show(ui, |ui| {
+                for (name, map) in &mut self.maps {
+                    if ui.checkbox(&mut map.visible, name).changed() {
+                        self.tile_manager.set_visible(name, map.visible);
+                    }
+                    if ui.button("🗑").on_hover_text("Delete Map").clicked() {
+                        to_delete.push(name.clone());
+                    }
+                    ui.end_row();
+                }
+            });
+        self.delete(&to_delete);
+
+        if self.maps.is_empty() {
+            return;
+        }
+
+        ui.separator();
+        ui.add_space(SPACE);
+        ui.heading("Pose");
+        ui.add_space(SPACE);
+        self.pose_edit(ui);
+        if !self.options.pose_edit.selected_map.is_empty() && self.maps.len() > 1 {
+            ui.separator();
+            ui.add_space(SPACE);
+            self.apply_pose_to_other_maps(ui);
+        }
     }
 }
