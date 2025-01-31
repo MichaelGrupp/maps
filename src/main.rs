996785f107b27ab666eeffb9079208b22c9bb6e1
Michael Grupp <michael.grupp@tum.de>
2025-01-31 12:05:50 +0100

    Fix window dimension in help string

--- src/main.rs
+++ src/main.rs
@@ -47,7 +47,7 @@ struct Args {
         num_args = 2,
         value_names = &["width", "height"],
         default_values_t = Vec::from(&[1500., 1000.]),
-        help = "Initial window width and height in pixels."
+        help = "Initial window width and height in screen points."
     )]
     window_size: Vec<f32>,
     #[clap(
