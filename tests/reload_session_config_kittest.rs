e5e59b5d1b46281784b162f2080d3ad57125f02b
Michael Grupp <michael.grupp@tum.de>
2025-02-04 21:21:18 +0100

    Add snapshot kittest for session & config file loading

---
+++ tests/reload_session_config_kittest.rs
@@ -0,0 +1,30 @@
+mod kittest_common;
+
+use std::path::PathBuf;
+
+use eframe::egui;
+
+use kittest_common::*;
+use maps::app::AppState;
+use maps::persistence::load_app_options;
+
+const OPTIONS_PATH: &str = "tests/sessions/custom_options.toml";
+const SESSION_PATH: &str = "tests/sessions/custom_session.toml";
+
+#[test]
+fn main() {
+    env_logger::init();
+
+    let app_options = load_app_options(&Some(PathBuf::from(OPTIONS_PATH)));
+
+    let mut app_state =
+        AppState::init(Vec::new(), app_options).expect("Failed to initialize AppState");
+
+    app_state.load_session(PathBuf::from(SESSION_PATH));
+
+    snapshot_full_app(
+        app_state,
+        "reload_session_config",
+        egui::Vec2::new(1000., 750.),
+    );
+}
