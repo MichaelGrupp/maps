9eb49f530255564858b1bd7f16f22f4d8ee1318c
Michael Grupp <michael.grupp@tum.de>
2025-02-05 18:46:00 +0100

    Use new HarnessBuilder::build_eframe for kittest

--- tests/kittest_common/mod.rs
+++ tests/kittest_common/mod.rs
@@ -1,8 +1,7 @@
 use std::path::PathBuf;
 
 use eframe::egui;
-use eframe::egui::Context;
-use egui_kittest::Harness;
+use egui_kittest::HarnessBuilder;
 
 use maps::app::AppState;
 use maps::meta::Meta;
@@ -18,42 +17,19 @@ const PIXELS_PER_POINT: f32 = 1.;
 /// Spins up the full app state UI.
 /// Does a snapshot diff test unless the "kittest_snapshots" feature is disabled.
 /// To create/update baseline snapshots, run: UPDATE_SNAPSHOTS=1 cargo test
-pub fn snapshot_full_app(mut app_state: AppState, test_name: &str, size: egui::Vec2) {
-    let app_closure = |ctx: &Context| {
-        // This is copypasta from AppState::update().
-        // TODO: refactor this once egui_kittest can run eframe::App.
-        // Waiting for this to be released:
-        // https://github.com/emilk/egui/commit/46b58e5bcca0bb34861b5671958be872419bee90
-        egui::CentralPanel::default().show(ctx, |ui| {
-            app_state.error_modal(ui);
-            app_state.quit_modal(ui);
-            app_state.handle_key_shortcuts(ui);
-
-            app_state.header_panel(ui);
-            app_state.menu_panel(ui);
-            app_state.footer_panel(ui);
-            app_state.settings_panel(ui);
-            app_state.central_panel(ui);
-
-            app_state.info_window(ui);
-        });
-
-        if ctx.input(|i| i.viewport().close_requested()) {
-            if app_state.status.unsaved_changes {
-                ctx.send_viewport_cmd(egui::ViewportCommand::CancelClose);
-                app_state.status.quit_modal_active = true;
-            }
-        }
-    };
-
-    let mut harness = Harness::builder()
+pub fn snapshot_full_app(app_state: AppState, test_name: &str, size: egui::Vec2) {
+    let mut harness = HarnessBuilder::default()
         .with_size(size)
         .with_pixels_per_point(PIXELS_PER_POINT)
-        .build(app_closure);
-    harness.run();
+        .build_eframe(|cc: &mut eframe::CreationContext| {
+            // This gives us image support:
+            // TODO: still needed? (see also main.rs)
+            egui_extras::install_image_loaders(&cc.egui_ctx);
+            app_state
+        });
 
     #[cfg(feature = "kittest_snapshots")]
-    harness.wgpu_snapshot(test_name);
+    harness.snapshot(test_name);
 
     #[cfg(not(feature = "kittest_snapshots"))]
     println!(
