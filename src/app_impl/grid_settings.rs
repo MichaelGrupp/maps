d83220360636887c50c718d705fc88cd20414b23
Michael Grupp <grupp@magazino.eu>
2025-02-17 16:10:05 +0100

    Hide grid line settings if they are deactivated

--- src/app_impl/grid_settings.rs
+++ src/app_impl/grid_settings.rs
@@ -12,9 +12,71 @@ impl AppState {
         }
         ui.add_space(SPACE);
         ui.end_row();
+
         ui.label("Show grid lines");
         ui.checkbox(&mut self.options.grid.lines_visible, "");
         ui.end_row();
+        if self.options.grid.lines_visible {
+            self.grid_lines_settings(ui);
+        }
+
+        ui.end_row();
+        ui.label("Show marker");
+        self.options.grid.marker_visibility.ui(ui);
+        ui.end_row();
+        if self.options.grid.marker_visibility.any_visible() {
+            ui.label("Marker length (meters)");
+            ui.add(egui::Slider::new(
+                &mut self.options.grid.marker_length_meters,
+                0.1..=25.0,
+            ));
+            ui.end_row();
+            ui.label("Marker width (meters)");
+            ui.add(egui::Slider::new(
+                &mut self.options.grid.marker_width_meters,
+                0.01..=5.,
+            ));
+            ui.end_row();
+            ui.label("Marker color (x, y, z)");
+            ui.horizontal(|ui| {
+                ui.color_edit_button_srgba(&mut self.options.grid.marker_x_color);
+                ui.color_edit_button_srgba(&mut self.options.grid.marker_y_color);
+                ui.color_edit_button_srgba(&mut self.options.grid.marker_z_color);
+            });
+            ui.end_row();
+        }
+        ui.end_row();
+        ui.label("Grid scale (points per meter)");
+        ui.add(egui::Slider::new(
+            &mut self.options.grid.scale,
+            self.options.grid.min_scale..=self.options.grid.max_scale,
+        ));
+        ui.end_row();
+        ui.label("Zoom delta (%)")
+            .on_hover_text("How much the grid zooms in/out when scrolling.");
+        ui.add(egui::Slider::new(
+            &mut self.options.grid.scroll_delta_percent,
+            0.01..=10.,
+        ));
+        ui.end_row();
+        ui.end_row();
+
+        ui.heading("Tools");
+        ui.add_space(SPACE);
+        ui.end_row();
+        ui.label("Measurement color")
+            .on_hover_text("Line color of the measurement tool.");
+        ui.color_edit_button_srgba(&mut self.options.grid.measure_stroke.color);
+        ui.end_row();
+        ui.label("Lens magnification")
+            .on_hover_text("Magnification factor for hovering / fixed lenses.");
+        ui.add(egui::Slider::new(
+            &mut self.options.grid.lens_magnification,
+            0.1..=10.0,
+        ));
+    }
+
+    fn grid_lines_settings(&mut self, ui: &mut egui::Ui) {
         ui.label("Show sub grid lines")
             .on_hover_text("Show sub lines between main grid lines.");
         ui.horizontal(|ui| {
@@ -94,59 +156,5 @@ impl AppState {
         ui.label("Tick label color");
         ui.color_edit_button_srgba(&mut self.options.grid.tick_labels_color);
         ui.end_row();
-        ui.end_row();
-        ui.label("Show marker");
-        self.options.grid.marker_visibility.ui(ui);
-        ui.end_row();
-        if self.options.grid.marker_visibility.any_visible() {
-            ui.label("Marker length (meters)");
-            ui.add(egui::Slider::new(
-                &mut self.options.grid.marker_length_meters,
-                0.1..=25.0,
-            ));
-            ui.end_row();
-            ui.label("Marker width (meters)");
-            ui.add(egui::Slider::new(
-                &mut self.options.grid.marker_width_meters,
-                0.01..=5.,
-            ));
-            ui.end_row();
-            ui.label("Marker color (x, y, z)");
-            ui.horizontal(|ui| {
-                ui.color_edit_button_srgba(&mut self.options.grid.marker_x_color);
-                ui.color_edit_button_srgba(&mut self.options.grid.marker_y_color);
-                ui.color_edit_button_srgba(&mut self.options.grid.marker_z_color);
-            });
-            ui.end_row();
-        }
-        ui.end_row();
-        ui.label("Grid scale (points per meter)");
-        ui.add(egui::Slider::new(
-            &mut self.options.grid.scale,
-            self.options.grid.min_scale..=self.options.grid.max_scale,
-        ));
-        ui.end_row();
-        ui.label("Zoom delta (%)")
-            .on_hover_text("How much the grid zooms in/out when scrolling.");
-        ui.add(egui::Slider::new(
-            &mut self.options.grid.scroll_delta_percent,
-            0.01..=10.,
-        ));
-        ui.end_row();
-        ui.end_row();
-
-        ui.heading("Tools");
-        ui.add_space(SPACE);
-        ui.end_row();
-        ui.label("Measurement color")
-            .on_hover_text("Line color of the measurement tool.");
-        ui.color_edit_button_srgba(&mut self.options.grid.measure_stroke.color);
-        ui.end_row();
-        ui.label("Lens magnification")
-            .on_hover_text("Magnification factor for hovering / fixed lenses.");
-        ui.add(egui::Slider::new(
-            &mut self.options.grid.lens_magnification,
-            0.1..=10.0,
-        ));
     }
 }
