f2da204a68b37b3aa33ce254c28477f20c91c552
Michael Grupp <michael.grupp@tum.de>
2025-05-25 14:09:45 +0200

    Improve documentation of internal grid module
    Also removes unnecessary id parameter for GridMapRelation and
    unnecessary "left_offset" hack (slightly changes snapshots).

--- src/grid.rs
+++ src/grid.rs
@@ -9,22 +9,30 @@ use crate::map_pose::MapPose;
 use crate::map_state::MapState;
 use crate::texture_request::{ImagePlacement, RotatedCropRequest, TextureRequest};
 
+/// Grid area for displaying metric objects in screen space (points).
 pub struct Grid {
     pub name: String,
+    /// Screen space offset of the grid's UI area.
     pub ui_offset: egui::Vec2,
+    /// Extent of the map in meters (width, height).
     pub metric_extent: egui::Vec2,
+    /// Display scale of the grid: how many points per meter?
     pub points_per_meter: f32,
-    pub origin_in_points: egui::Pos2, // Location of the origin in point coordinates.
-    pub left_offset: egui::Vec2,
+    /// Location of the origin in point coordinates.
+    pub origin_in_points: egui::Pos2,
     texture_crop_threshold: u32,
 }
 
-// Relations of a RHS metric coordinate map to the LHS point coordinate grid.
+/// Relations of a RHS metric coordinate map to the LHS point coordinate grid.
 struct GridMapRelation {
+    /// Size of the map scaled to the grid's zoom level, in points.
     scaled_size: egui::Vec2,
+    /// Points occupied by a cell (pixel) of the map.
     points_per_cell: f32,
-    ulc_to_origin_in_points: egui::Vec2, // Upper left corner to map origin in points.
-    ulc_to_origin_in_points_translated: egui::Vec2, // Includes map pose translation.
+    /// Upper left image corner to map origin in points, without map pose translation.
+    ulc_to_origin_in_points: egui::Vec2,
+    /// Upper left image corner to map origin in points, including map pose translation.
+    ulc_to_origin_in_points_translated: egui::Vec2,
 }
 
 /// Switches from left- to right-handed coordinate system or vice versa.
@@ -34,18 +42,11 @@ fn flip(vec: egui::Vec2) -> egui::Vec2 {
 }
 
 impl GridMapRelation {
-    fn new(grid: &Grid, map: &mut MapState, id: &str) -> GridMapRelation {
+    fn new(grid: &Grid, map: &mut MapState) -> GridMapRelation {
         let points_per_meter = 1. / map.meta.resolution;
         let scale_factor = grid.points_per_meter / points_per_meter;
         let points_per_cell = points_per_meter * map.meta.resolution * scale_factor;
-
-        let texture_state = map.get_or_create_texture_state(id);
-
-        let original_size = egui::Vec2::new(
-            texture_state.image_pyramid.original.width() as f32,
-            texture_state.image_pyramid.original.height() as f32,
-        );
-        let scaled_size = original_size * scale_factor;
+        let scaled_size = map.image_pyramid.original_size * scale_factor;
 
         // Meta origin is lower left corner of image in ROS.
         let llc_to_origin_in_points = flip(map.meta.origin_xy) * points_per_meter * scale_factor;
@@ -69,6 +70,8 @@ struct LabelTextOptions {
 }
 
 impl Grid {
+    /// Creates a new grid to be drawn in the available space of the given `ui`
+    /// with the desired scale defined by `points_per_meter`.
     pub fn new(ui: &egui::Ui, name: &str, points_per_meter: f32) -> Grid {
         // Where are we with this UI in the global context?
         // Required to offset the origin because we paint at manual positions.
@@ -76,42 +79,47 @@ impl Grid {
 
         let available_size = ui.available_size();
         let metric_extent = available_size * points_per_meter;
-        // TODO: offset is a hack to avoid wrong drawing when a left side menu is expanded.
-        let left_offset = egui::vec2(ui.cursor().min.x, 0.);
         Grid {
             name: name.to_string(),
             ui_offset,
             metric_extent,
             points_per_meter,
             origin_in_points: (available_size / 2.).to_pos2() + ui_offset,
-            left_offset,
             texture_crop_threshold: 0,
         }
     }
 
+    /// Sets the position offset of the grid's zero origin, in points.
     pub fn with_origin_offset(mut self, offset: egui::Vec2) -> Self {
         self.origin_in_points += offset;
         self
     }
 
+    /// Fixates the grid to be displayed with the specified metric position in the UI center.
     pub fn centered_at(self, metric_pos: egui::Pos2) -> Self {
         let offset = flip(-metric_pos.to_vec2()) * self.points_per_meter;
         self.with_origin_offset(offset)
     }
 
+    /// Sets the threshold for texture cropping, i.e. the max texture width/height that can
+    /// be displayed without cropping.
+    /// Set this threshold to allow displaying images larger than the GPU texture buffer.
     pub fn with_texture_crop_threshold(mut self, threshold: u32) -> Self {
         self.texture_crop_threshold = threshold;
         self
     }
 
+    /// Converts a point grid coordinate to a metric coordinate.
     pub fn to_metric(&self, point: &egui::Pos2) -> egui::Pos2 {
         (flip(*point - self.origin_in_points) / self.points_per_meter).to_pos2()
     }
 
+    /// Converts a metric grid coordinate to points.
     pub fn to_point(&self, metric: &egui::Pos2) -> egui::Pos2 {
         flip(metric.to_vec2()).to_pos2() * self.points_per_meter + self.origin_in_points.to_vec2()
     }
 
+    /// Adds a single map to be displayed.
     pub fn show_map(
         &self,
         ui: &mut egui::Ui,
@@ -123,7 +131,7 @@ impl Grid {
             return;
         }
 
-        let relation = GridMapRelation::new(self, map, self.name.as_str());
+        let relation = GridMapRelation::new(self, map);
 
         let rect = egui::Rect::from_min_size(
             self.origin_in_points + relation.ulc_to_origin_in_points,
@@ -163,6 +171,7 @@ impl Grid {
         }
     }
 
+    /// Adds multiple maps to be displayed in the specified draw order.
     pub fn show_maps(
         &self,
         ui: &mut egui::Ui,
@@ -179,6 +188,7 @@ impl Grid {
         }
     }
 
+    /// Returns the mouse pointer position in the grid's metric space, if hovered.
     pub fn hover_pos_metric(&self, ui: &egui::Ui) -> Option<egui::Pos2> {
         if !ui.rect_contains_pointer(ui.clip_rect()) {
             return None;
@@ -271,7 +281,7 @@ impl Grid {
         line_type: &LineType,
         label_text_options: &Option<LabelTextOptions>,
     ) {
-        let left = egui::Pos2::new(0., y) + self.left_offset;
+        let left = egui::Pos2::new(self.ui_offset.x, y);
         let stroke = match line_type {
             LineType::Main => &options.line_stroke,
             LineType::Sub => &options.sub_lines_stroke,
@@ -279,7 +289,7 @@ impl Grid {
         ui.painter().line_segment(
             [
                 left,
-                egui::Pos2::new(ui.available_width() + self.ui_offset.x, y) + self.left_offset,
+                egui::Pos2::new(ui.available_width() + self.ui_offset.x, y),
             ],
             *stroke,
         );
@@ -300,6 +310,7 @@ impl Grid {
         }
     }
 
+    /// Draws vertical & horizontal grid lines according to the desired options and line type.
     pub fn draw(&self, ui: &mut egui::Ui, options: &GridOptions, line_type: LineType) {
         if !options.lines_visible {
             return;
@@ -328,6 +339,7 @@ impl Grid {
         self.draw_horizontal_lines(ui, options, &line_type, spacing_points, &label_text_options);
     }
 
+    /// Draws XYZ coordinate axes at the pose.
     pub fn draw_axes(&self, ui: &mut egui::Ui, options: &GridOptions, pose: Option<&MapPose>) {
         // Convert stroke width to points.
         let x_stroke = egui::Stroke::new(
@@ -363,6 +375,8 @@ impl Grid {
         );
     }
 
+    /// Draws the currently active measurement from the `options`, if it exists.
+    /// Set `temporary_end` to display an unfinished measurement.
     pub fn draw_measure(
         &self,
         ui: &mut egui::Ui,
