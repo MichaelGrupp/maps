798898cd8e74ffbaa704194310f3ed0914e352b8
Michael Grupp <michael.grupp@tum.de>
2025-10-11 11:28:14 +0200

    Simplify maps_io_ros demo

--- crates/maps_io_ros/examples/demo.rs
+++ crates/maps_io_ros/examples/demo.rs
@@ -6,7 +6,7 @@ use std::path::{Path, PathBuf};
 use std::process::exit;
 
 use image::DynamicImage;
-use maps_io_ros::{ColorMap, Meta, load_image};
+use maps_io_ros::{ColorMap, Meta, load_image, save_image};
 
 fn filename(path: &Path) -> &str {
     path.file_name()
@@ -14,27 +14,16 @@ fn filename(path: &Path) -> &str {
         .unwrap_or("unknown")
 }
 
-fn main() {
-    let meta_path = PathBuf::from(std::env::args().nth(1).unwrap_or_else(|| {
-        eprintln!("Usage: demo <map.yaml>");
-        exit(1);
-    }));
-
+fn demo(meta_path: &Path) -> maps_io_ros::Result<()> {
     // Load the map metadata from the yaml file.
-    let meta = Meta::load_from_file(&meta_path).unwrap_or_else(|e| {
-        eprintln!("Error loading map yaml {meta_path:?}: {e}");
-        exit(1);
-    });
+    let meta = Meta::load_from_file(&meta_path)?;
     println!(
         "Loaded map metadata: {:?} (image path: {:?})",
         meta_path, meta.image_path
     );
 
     // Load the map image from the path specified in the metadata.
-    let img = load_image(&meta.image_path).unwrap_or_else(|e| {
-        eprintln!("Error loading map image {:?}: {}", meta.image_path, e);
-        exit(1);
-    });
+    let img = load_image(&meta.image_path)?;
     println!("Loaded map image from {:?}", meta.image_path);
 
     // Like in RViz, colormaps operate on RGBA image buffers.
@@ -50,9 +39,24 @@ fn main() {
 
     // Save the resulting image to a temporary file.
     let out_path = std::env::temp_dir().join("processed_".to_owned() + filename(&meta.image_path));
-    img_rgba.save(&out_path).unwrap_or_else(|e| {
-        eprintln!("Error saving processed image to {out_path:?}: {e}");
-        exit(1);
-    });
+
     println!("Saved processed image to {out_path:?}");
+    save_image(&out_path, &img_rgba)?;
+
+    Ok(())
+}
+
+fn main() {
+    let meta_path = PathBuf::from(std::env::args().nth(1).unwrap_or_else(|| {
+        eprintln!("Usage: demo <map.yaml>");
+        exit(1);
+    }));
+
+    #[cfg(debug_assertions)]
+    println!("You're running the demo in debug mode! Expect poor performance.");
+
+    if let Err(e) = demo(&meta_path) {
+        eprintln!("{e}");
+        exit(1);
+    }
 }
